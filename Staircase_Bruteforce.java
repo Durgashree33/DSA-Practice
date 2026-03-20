4f2e11e078f6cf07c3bedd780e694d8468ed8983
Durgashree <durgashreepoojary@gmail.com>
2026-03-20 23:48:05 +0530

    Add staircase bruteforce method

---
+++ Staircase_Bruteforce.java
@@ -0,0 +1,21 @@
+public class Staircase_Bruteforce {
+    public static void main(String[] args) {
+
+        int n = 5; // height of staircase
+
+        for (int i = 1; i <= n; i++) {
+
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // print hashes
+            for (int k = 1; k <= i; k++) {
+                System.out.print("#");
+            }
+
+            System.out.println(); // new line
+        }
+    }
+}
